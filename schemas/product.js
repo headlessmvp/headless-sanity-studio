e57e96d4174cd1a56b535064d6510fba3048d8e7
Developer Junaid <qjunaid623@gmail.com>
2022-09-07 12:18:36 +0500

    added basic schema

---
+++ schemas/product.js
@@ -0,0 +1,12 @@
+export default {
+  name: "product", // To access, use : _type: "animal"
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+  ],
+}
